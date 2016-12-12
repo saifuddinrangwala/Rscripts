a79d6f2e1c3af0954d49c6a8cbac2cd5f53f99ea
Rangwala <srangwal@bmc.com>
2016-12-12 22:14:22 +0530

    Add revised R script for unemployment test

--- annova/Test - Unemployment.R
+++ annova/Test - Unemployment.R
@@ -21,7 +21,7 @@ boxplot(Unemployment$unemp ~ Unemployment$month)
 boxplot(Unemployment$unemp ~ Unemployment$year)
 
 # Super imposed denisty plot by factor
-qplot(Unemployment$unemp, colour=Unemployment$month, data=Unemployment, geom="density")
+qplot(Unemployment$unemp, colour=Unemployment$year, data=Unemployment, geom="density")
 
 # Get the descritives table by each group
 describeBy(Unemployment$unemp, Unemployment$month)
@@ -66,3 +66,48 @@ plot.design(Unemployment)
 # Get the interaction plot
 interaction.plot(Unemployment$year, Unemployment$month, Unemployment$unemp, col=2:50, lty=1)
 interaction.plot(Unemployment$month, Unemployment$year, Unemployment$unemp, col=2:14, lty=1)
+
+# Obviously there is actually only one factor here which is time, so two way annova won't give desired results, one way annova with year is sufficient
+
+
+Unemployment_lm <- lm(Unemployment$unemp ~ Unemployment$year, data = Unemployment)
+Unemployment_lm_summary = summary(Unemployment_lm)
+Unemployment_lm_summary
+if (Unemployment_lm_summary$r.squared < 0.5)
+{
+  print("R- squared value is very less, which means using analysis of variance will not help here")
+}
+
+
+Unemployment_aov <- aov(Unemployment_lm)
+Unemployment_aov_summary = summary(Unemployment_aov)
+Unemployment_aov_summary
+
+Unemployment_residual = residuals(Unemployment_aov)
+Unemployment_fitted = fitted(Unemployment_aov)
+
+# Get the residual vs fitted plot i.e. check how close are observed values to fitted values
+# http://blog.minitab.com/blog/adventures-in-statistics/regression-analysis-how-do-i-interpret-r-squared-and-assess-the-goodness-of-fit
+plot(Unemployment_fitted, Unemployment_residual)
+abline(h=0, lty=2)
+lines(smooth.spline(Unemployment_fitted, Unemployment_residual))
+
+#Box plot across multiple factors
+#boxplot(Unemployment$unemp ~ Unemployment$month * Unemployment$year, data = Unemployment)
+#boxplot(Unemployment_residual ~ Unemployment$month * Unemployment$year, data = Unemployment)
+
+# Now run tukey's range test to analyze variations in means across individual groups
+tk <- TukeyHSD(Unemployment_aov)
+
+# List the tukey's table
+tk
+
+# Plot to see variations in the means for each group pair
+plot(tk)
+
+# Get the means plot
+plot.design(Unemployment)
+
+boxplot(Unemployment$unemp ~ Unemployment$year)
+
+
